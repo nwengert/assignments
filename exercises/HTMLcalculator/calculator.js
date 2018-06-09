2da8fed2646bdf02025b29727af493c56e6ef856
Nathan Wengert <nate.went@gmail.com>
2018-06-08 23:43:25 -0600

    finished a few more exercises

--- exercises/HTMLcalculator/calculator.js
+++ exercises/HTMLcalculator/calculator.js
@@ -1,23 +1,40 @@
 
+
+
 document.getElementById("addbutton").addEventListener("click", function (e) {
     e.preventDefault()
     var FirstAdd = document.getElementById("addone").valueAsNumber;
     var SecondAdd = document.getElementById("addtwo").valueAsNumber;
-    alert(FirstAdd + SecondAdd);
+
+    var mathSpot = document.getElementById("addform");
+    var answer = document.createElement("span");
+    answer.innerText = FirstAdd + SecondAdd;
+    mathSpot.appendChild(answer);
 })
 
 document.getElementById("subtractbutton").addEventListener("click", function (e) {
     e.preventDefault()
     var FirstSubtract = document.getElementById("subone").valueAsNumber;
     var SecondSubtract = document.getElementById("subtwo").valueAsNumber;
-    alert(FirstSubtract - SecondSubtract);
+
+    var mathSpot = document.getElementById("subtractform");
+    var answer = document.createElement("span");
+    answer.innerText = FirstSubtract - SecondSubtract;
+    mathSpot.appendChild(answer);
+    console.log(FirstSubtract - SecondSubtract);
 })
 
 document.getElementById("multiplybutton").addEventListener("click", function (e) {
     e.preventDefault()
     var Firstmultiply = document.getElementById("multOne").valueAsNumber;
     var Secondmultiply = document.getElementById("multTwo").valueAsNumber;
-    alert(Firstmultiply * Secondmultiply);
+    var mathSpot = document.getElementById("multiplyform");
+    var answer = document.createElement("span");
+    answer.innerText = Firstmultiply * Secondmultiply;
+    mathSpot.appendChild(answer);
+
+    console.log(Firstmultiply * Secondmultiply);
 })
+
 
 
--- exercises/loops-exercise.js
+++ exercises/loops-exercise.js
@@ -1,10 +1,14 @@
-// Write a function that accepts a string as input. Write a loop to print out each letter of that string individually.
+// Write a function that accepts a string as input. 
+// Write a loop to print out each letter of that string individually.
 
-// NOT DONE  ----I don't know how to make this into a function
-var str = 'avacado'
-for (var i = 0; i < str.length; i++) {
-    console.log(str.charAt(i));
+//DONE
+var myString = 'boomerang'
+var printLetters = function(str) {
+    for (var i = 0; i < str.length; i++) {
+        console.log(str.charAt(i));
+    }
 }
+printLetters(myString);
 
 //   Write a function that accepts a string and a single character as inputs. 
 //   Write a loop that iterates over the string, and prints the position of
@@ -15,7 +19,7 @@ for (var i = 0; i < str.length; i++) {
 function Segundo(str, singleCharacter) {
     for (var i = 0; i<str.length; i++) {
         if (str.includes(singleCharacter)) {
-            return('the character is at position ' + str.indexOf(singleCharacter));JSON
+            return(`the character is at position ${str.indexOf(singleCharacter)}`);
         } else {
             return("character not found");
         }
@@ -23,40 +27,32 @@ function Segundo(str, singleCharacter) {
 }
 console.log(Segundo('Windowpane', 'd'));
 
-
 // Write a function that accepts an array of numbers as a parameter. 
 // Quit the function when the number 42 is found. 
 // Let the user know if 42 is not found.
 
-// NOT DONE ---- yo no se
+//DONE
+var miArray = [33, 44, 56, 67, 78, 42, 33, 12, 64];
 
-
-function fortyTwo(arr) {
-var miArray = [13,54,17,22,54,87,56,42,37,44,12];
-    for (i = 0; i < miArray.length; i++) {
-        if (miArray[i] == 42) {
-            return('the number 42 was found at position ' + miArray[i]);
-        } else {
-            return('the number 42 was NOT found');
+var foundFortyTwo = function (arr) {
+    for(i=0; i<=arr.length; i++) {
+        if (arr[i]===42) {
+            break;
         }
-    }
+    } return('42 was found at index ' + i);
 }
-console.log(fortyTwo(miArray));
+
+console.log(foundFortyTwo(miArray));
 
 
 // Write a function that accepts 10 numbers in an array and then 
 // prints out the smallest number out of the ten without using Math.min()
 
-// NOT DONE
-    var thisArray = [5, 17, 23, 22, 32, 42, 93, 27, 46, 61];
-    var j = thisArray[0]
-
-    function findSmallest(thisArray) {
-        for (i = 1; i < thisArray.length; i++) {
-            if (j < i) {
-                i = j;
-            }
-        }
-        return(i);
-    }
-    console.log(findSmallest(thisArray))
+//DONE - though worth looking over again. 
+function sortNumber(a,b) {
+    return a - b;
+}
+    var numArray = [17, 23, 22, 32, 11, 42, 93, 66, 27, 46, 61];
+    numArray.sort(sortNumber);
+    numArray.join(",");
+    console.log('the smallest number is ' + numArray[0]);
