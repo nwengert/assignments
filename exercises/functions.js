d78b8c82893dcbad9e843d96e562c0b4ed64ef65
Nathan Wengert <nate.went@gmail.com>
2018-06-10 21:56:53 -0600

    A few things over the weekend

--- exercises/functions.js
+++ exercises/functions.js
@@ -2,21 +2,26 @@
 // Write a function that accepts two numbers 
 // as parameters, and returns the sum.
 
+//DONE
 var sumificar = function(x,y) {
     return x + y;
 }
 console.log(sumificar(7,8));
 
-
-
-
 // Write a function that accepts three numbers as 
 // parameters, and returns the largest of those numbers.
 
+// DONE
 var largestNum = function (x,y,z) {
-    return Math.max(x,y,z);
+    if (x > y && x > z) {
+        return x;
+    } else if (y > x && y > z) {
+        return y;
+    } else {
+        return z;
+    }
 }
-console.log(largestNum(34,74,22));
+console.log('The largest of the three is: ' + largestNum(34,11,22));
 
 
 
@@ -25,10 +30,11 @@ console.log(largestNum(34,74,22));
 // and returns whether that number is even or odd. 
 // (Return the string "even" or "odd");
 
+//DONE
 var parImpar = function(j) {
     return j % 2 ? 'odd' : 'even';
 }
-console.log(parImpar(445));
+console.log('The number is ' + parImpar(445));
 
 
 
@@ -41,6 +47,7 @@ console.log(parImpar(445));
 // If the string is more than twenty characters long, 
 // return the first half of the string.
 
+//DONE
 var stringLength = function (hilo) {
     if (hilo.length <= 20) {
         return hilo = (hilo + hilo);
@@ -48,26 +55,20 @@ var stringLength = function (hilo) {
         return hilo.slice(0,20);
     }
 }
-console.log(stringLength('First I was afraid, I was petrified.  Kept thinking I could never live'));
-
-
-
-
+console.log(stringLength('First I was '));
 
 // Write a function that accepts a number ‘n’ as a parameter. 
 // Then print the first ‘n’ Fibonacci numbers and return their sum.
     // IS THIS A FACTORIAL QUESTION?
 
-var fibo = function (n) {
-    var a = 0, b = 0, i = 0;
-    for (i = n-1; i >= 0; i--) {
-        a = n - i;
-        console.log(a);
-    }
-    console.log(b);
-    }
-
-fibo(6);
+// var fibo = function (n) {
+//     y = n;
+//     for (i = n - 1; i > 0; i--) {
+//         var y = y + i;
+//         console.log()
+//     }
+// }
+// fibo(6);
 
 
 // Write a function to solve the quadratic equation.
@@ -75,11 +76,19 @@ fibo(6);
 //     and then return an array with the resulting x values.)
 // NOT DONE
 
-var quadraticEq = function(a, b, c) {
-  var equis = (-b +- )
+// var quadraticEq = function(a, b, c) {
+//   var equis = (-b +- );
 
-}
+// }
 
 // Write a function that accepts a string as a parameter. 
 // Return the most frequently occuring letter in that string.
+
 // NOT DONE
+  //these are kindof hard. 
+var mostFreqLetter = function(str) {
+    return str.split('').sort();
+    //loop through the array until the letter changes
+    //   count how many loops until the letter changes
+}
+console.log(mostFreqLetter('nathan'));
