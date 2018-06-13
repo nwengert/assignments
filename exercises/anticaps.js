92208c555af46fdf13689869c10f525c50e970e5
Nathan Wengert <nate.weng@gmail.com>
2018-06-12 23:06:24 -0600

    A few more exercises completed

--- exercises/anticaps.js
+++ exercises/anticaps.js
@@ -1,18 +1,18 @@
 // Write a function called antiCaps which accepts a string 
 // and returns a string. The purpose of the function is 
 // to invert the capitalization of each letter in a string.
-function isCaps(letter) {  
-    return letter === letter.toUpperCase();
-  } 
-var antiCaps = function(str) {
-     var newStr = str.split("");
-     var newerStr = [];
-     for (i = 0; i < newStr.length; i++) {
-         if(isCaps(newStr[i])) {
-             newerStr.push(newStr[i].toLowerCase);
-         }else{
-             newerStr.push(newStr[i].toUpperCase);
-         }
-     }return newerStr;
- }
- console.log(antiCaps('This is that ice cold'))
+
+//DONE
+  var antiCaps = function(str) {
+    newStr = str.slice("");
+    myArray = []
+    for(i = 0; i < newStr.length; i++) {
+        if(newStr[i] === newStr[i].toUpperCase()) {
+            myArray.push(newStr[i].toLowerCase());
+        }else{
+            myArray.push(newStr[i].toUpperCase());
+        }
+    } 
+    return myArray.join("");
+  }
+console.log(antiCaps('My Name is Nathan Wengert'));
