8e024a75473b5eb42b3c01a630211bfe7c697bd3
Nathan Wengert <nate.weng@gmail.com>
2018-08-26 19:54:56 -0600

    course tidyup

--- projects/css-zen-garden/src/index.js
+++ projects/css-zen-garden/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/stylesBig.css'
-import './styles/stylesSmall.css';
+//styles
+// import './styles/stylesBig.css'
+// import './styles/stylesSmall.css';
+import './styles/Body.css';
+import './styles/Footer.css';
+import './styles/midCent.css';
+import './styles/Top.css';
+import './styles/viewThis.css';
+
 
 ReactDOM.render(
     <App />,
