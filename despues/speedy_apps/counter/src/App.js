a33ea8ef296684902442ec849c8f41f620de5d6a
Nathan Wengert <nate.weng@gmail.com>
2018-12-07 18:26:19 -0700

    rinse and repeat

---
+++ despues/speedy_apps/counter/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(prevState) {
+        this.setState(prevState =>{
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+
+    render(){
+        return(
+            <div className='wrapper'>
+                <h1>{this.state.count}</h1>
+                <button 
+                    onClick={this.handleClick}
+                >Click</button>
+            </div>
+        )
+    }
+}
+
+export default App
