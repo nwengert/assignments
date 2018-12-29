87b1aad396472bc6ee20d54a6382d71d0828e951
Nathan Wengert <nate.weng@gmail.com>
2018-12-29 13:33:12 -0700

    practicing

---
+++ despues/speedy_apps/counter/src/Aqqqqq.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+export default class Aqqqqq extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        this.increment = this.increment.bind(this)
+        this.decrement = this.decrement.bind(this)
+        this.double = this.double.bind(this)
+    }
+    increment() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+    decrement() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count -1
+            }
+        })
+    }
+    double() {
+        this.setState(prevState => ){
+            return{
+                count: prevState.count * 2
+            }
+        }
+    }
+    render() {
+        return(
+            <div>
+            <h1>{this.state.count}</h1>
+                <button onClick = {this.increment}>Increase</button>
+                <button onClick = {this.decrement}>Decrease</button>
+                <button onClick = {this.double}>Double</button>
+            </div>
+        )
+    }
+}
