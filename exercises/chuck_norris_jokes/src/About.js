464544904f935a012c31fe8170f8a80b70f9e92a
Nathan Wengert <nate.weng@gmail.com>
2019-01-08 20:42:31 -0700

    new component

---
+++ exercises/chuck_norris_jokes/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About (props) {
+    return (
+        <div>
+            <h1>ABOUT this app</h1>
+            <p>This app is literally a joke</p>
+        </div>
+    )
+}
+
+export default About
